fix accuracy calculation for batched data

training.py
@@ -106,11 +106,12 @@ def train(model, dataloaders, epochs, lr, epochs_til_checkpoint, model_dir, loss
                     writer.add_scalar("loss/total_train_loss", train_loss, total_steps)
 
                 ##### ACCURACY #####
-                obj_class = int(gt['class'].cpu().numpy())
-                predicted_class = int(np.argmax(model_output['class'].cpu().numpy()))
-                predicted_class = multiclass_dataio.class2string_dict[predicted_class]
-                is_class_correct = 1 if predicted_class == obj_class else 0
-                class_prediction[obj_class].append(is_class_correct)
+                for i in range(gt['class'].shape[0]): # gt is a batch of samples -> need to iterate through dimension 0
+                    obj_class = int(gt['class'][i].cpu().numpy())
+                    predicted_class = int(np.argmax(model_output['class'][i].cpu().numpy()))
+                    predicted_class = multiclass_dataio.class2string_dict[predicted_class]
+                    is_class_correct = 1 if predicted_class == obj_class else 0
+                    class_prediction[obj_class].append(is_class_correct)
 
                 if not total_steps % steps_til_summary and rank == 0:
                     torch.save(model.state_dict(),