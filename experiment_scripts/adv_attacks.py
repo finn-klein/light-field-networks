88aa31abe2b83d416484202002c9754907480e02
finn-klein <finn.klein@outlook.com>
2022-07-20 10:27:53 +0200

    fix foolbox import path

--- experiment_scripts/adv_attacks.py
+++ experiment_scripts/adv_attacks.py
@@ -3,7 +3,7 @@ import sys
 import os
 sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ) )
 
-sys.path.append("../../foolbox")
+sys.path.append("../../foolbox/foolbox")
 import foolbox as fb
 import configargparse
 import os, time, datetime
