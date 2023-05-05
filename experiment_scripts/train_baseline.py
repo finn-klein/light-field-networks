35330a7eadf4a37a60e8d94422cdf9bb2edceac8
Finn Klein <finn.klein@outlook.com>
2023-05-05 09:45:40 +0200

    add train baseline script

--- experiment_scripts/train_baseline.py
+++ experiment_scripts/train_baseline.py
@@ -10,7 +10,26 @@ import matplotlib.pyplot as plt
 import time
 import os
 import copy
-print("PyTorch Version: ",torch.__version__)
-print("Torchvision Version: ",torchvision.__version__)
+import configargparse
 
-model = models.
+p = configargparse.ArgumentParser()
+p.add('-c', '--config_filepath', required=False, is_config_file=True)
+
+p.add_argument('--logging_root', type=str, default=config.logging_root)
+p.add_argument('--root_dir', type=str, required=True)
+p.add_argument('--batch_size', type=int, default=64)
+
+opt = p.parse_args()
+
+# Load dataset
+dataset = datasets.ImageFolder(opt.root_dir)
+num_classes = len(dataset.find_classes(opt.root_dir))
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batch_size, shuffle=True, num_workers=0)
+
+device = torch.device("cuda:0" if torch.cuda_is_available() else "cpu")
+
+# Load model
+model = models.resnet50(pretrained=True)
+# Replace last layer
+model.fc = nn.Linear(512, num_classes)
+
