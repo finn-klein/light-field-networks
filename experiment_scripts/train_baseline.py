088609d2b4cd0168e66472187c08940d7d9cccec
finn-klein <finn.klein@outlook.com>
2023-05-08 10:27:18 +0200

    use lists in collate

--- experiment_scripts/train_baseline.py
+++ experiment_scripts/train_baseline.py
@@ -33,7 +33,7 @@ dloaders = dict()
 def collate(data):
     d = [(np.asarray(sample), idx) for sample, idx in data]
     d = list(zip(*d))
-    return d[0], d[1]
+    return list(d[0]), list(d[1])
 
 # Load datasets
 dsets['train'] = datasets.ImageFolder(opt.root_dir_train)
