4ea62fa201db4a196cf67e30dae68c075162dcde
Finn Klein <finn.klein@outlook.com>
2023-06-05 14:00:33 +0200

    fix out path

--- experiment_scripts/single_class_lfn_test.sh
+++ experiment_scripts/single_class_lfn_test.sh
@@ -10,7 +10,7 @@ module load git
 
 conda activate lf
 
-out_path="/home/work/iwi9/iwi9015h/experiments/batchtest"
+out_path="/home/woody/iwi9/iwi9015h/experiments/batchtest"
 script_path="/home/woody/iwi9/iwi9015h/light-field-networks/experiment_scripts"
 lfn_root_path="/home/woody/iwi9/iwi9015h/nmr/NMR_Dataset"
 lfn_ckpt_path="/home/vault/iwi9/iwi9015h/experiments/train00/nmr/64_128_None/checkpoints/model_epoch_0006_iter_040000.pth"
