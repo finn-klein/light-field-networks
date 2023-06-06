0b624d201c09348a1d468ef851112fb182b809e1
Finn Klein <finn.klein@outlook.com>
2023-06-06 17:51:49 +0200

    add

---
+++ experiment_scripts/single_class_ff_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=lfn_2_l2gauss
+#SBATCH --time=01:00:00
+#SBATCH --gres=gpu:a100:1 --partition=a100
+#SBATCH --export=NONE
+
+module load python
+module load git
+
+conda activate lf
+
+out_path="/home/woody/iwi9/iwi9015h/experiments/batchtest"
+script_path="/home/woody/iwi9/iwi9015h/light-field-networks/experiment_scripts"
+lfn_root_path="/home/woody/iwi9/iwi9015h/nmr/NMR_Dataset"
+lfn_ckpt_path="/home/vault/iwi9/iwi9015h/experiments/train00/nmr/64_128_None/checkpoints/model_epoch_0006_iter_040000.pth"
+ff_root_path="/home/woody/iwi9/iwi9015h/nmr_ff_test"
+ff_ckpt_path="/home/vault/iwi9/iwi9015h/experiments/train_baseline00/checkpoints/best.pth"
+
+cd /home/woody/iwi9/iwi9015h/light-field-networks/
+python experiment_scripts/adv_attack_resnet.py --data_root $ff_root_path --checkpoint_path $ff_ckpt_path --attack_name l2gauss \
+ --single_class_string 2 --out_file ${out_path}/ff_2_l2gauss.txt
