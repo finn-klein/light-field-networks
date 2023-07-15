d95042f40362400da5462d1a720044a9f140191b
finn-klein <finn.klein@outlook.com>
2023-07-15 12:51:46 +0200

    remove superfluous argument

--- experiment_scripts/lfn_gradient_attacks.sh
+++ experiment_scripts/lfn_gradient_attacks.sh
@@ -18,6 +18,5 @@ python experiment_scripts/lfn_adversarial.py --data_root /home/woody/iwi9/iwi901
 --num_instances_per_class 256 \
 --batch_size 256 \
 --lr 1e-4 \
---attack_name doesntmatter \
 --single_class_string 0
 #done
