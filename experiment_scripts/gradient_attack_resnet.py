60f8c7578231fdcafb403d8ab94c2ce6a63ca462
finn-klein <finn.klein@outlook.com>
2023-08-23 11:46:17 +0200

    add

---
+++ experiment_scripts/gradient_attack_resnet.py
@@ -0,0 +1,124 @@
+# Enable import from parent package
+import sys
+import os
+sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ) )
+
+sys.path.append("/home/woody/iwi9/iwi9015h/foolbox")
+sys.path.append("/home/woody/iwi9/iwi9015h/eagerpy")
+import foolbox as fb
+import configargparse
+import os, time, datetime
+import config
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
+import torchvision.models as models
+from torchvision import datasets
+from torch.utils.data import DataLoader
+from models import LFAutoDecoder
+import util
+
+p = configargparse.ArgumentParser()
+p.add('-c', '--config_filepath', required=False, is_config_file=True)
+p.add("--checkpoint_path", required=True)
+p.add("--data_root", required=True)
+p.add("--batch_size", default=256)
+p.add("--out_file", type=str, required=False)
+p.add("--single_class_string", type=str, required=True)
+p.add('--max_num_instances', type=int, default=256)
+opt = p.parse_args()
+
+if opt.out_file is not None:
+    out_file = open(opt.out_file, "w")
+
+# helper function to get numpy array from PIL image
+def collate(data):
+    d = list(zip(*data))
+    imgs = d[0]
+    labels = d[1]
+    
+    # cast PIL.Image into np.array, normalize to [0; 1]
+    imgs = [np.asarray(img, dtype="float32") / 255 for img in imgs]
+    # convert list of imgs to np.array before wrapping in tensor (performance reasons)
+    imgs = np.array(imgs)
+    imgs = torch.tensor(imgs)
+    # permute dimensions as expected by ResNet
+    imgs = torch.permute(imgs, (0, 3, 1, 2)) # channels, rgb, x, y
+    
+    labels = torch.tensor(labels)
+    
+    return imgs, labels
+
+
+# Initialize dataset
+dataset = datasets.ImageFolder(opt.data_root)
+
+
+if opt.single_class_string is not None:
+    single_class_dataset = [(item, label) for (item, label) in dataset if label==int(opt.single_class_string)]
+    if opt.max_num_instances > len(single_class_dataset):
+        print(f"only {len(single_class_dataset)} are available.")
+    else:
+        single_class_dataset = single_class_dataset[:opt.max_num_instances]
+    dataloader = torch.utils.data.DataLoader(single_class_dataset, batch_size=opt.batch_size, shuffle=True, num_workers=0, collate_fn=collate)
+else:
+    if opt.max_num_instances > len(dataset):
+        print(f"only {len(dataset)} instances are available.")
+    else:
+        dataset = dataset[:opt.max_num_instances]
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batch_size, shuffle=True, num_workers=0, collate_fn=collate)
+num_classes = len(dataset.find_classes(opt.data_root)[0])
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = models.resnet50().eval()
+model = model.to(device)
+in_feat = model.fc.in_features
+# modify last layer
+model.fc = torch.nn.Linear(in_feat, num_classes)
+
+state_dict = torch.load(opt.checkpoint_path)
+model.load_state_dict(state_dict)
+# preprocessing??
+fmodel = fb.PyTorchModel(model, bounds=(0, 1))
+
+
+robust_accs = list()
+for imgs, labels in dataloader:
+    imgs = imgs.to(device)
+    labels = labels.to(device)
+    attack = fb.attacks.L2ProjectedGradientAttack(steps=100), random_start=False)
+    # epsilons = [
+    #     0.0,
+    #     0.0002,
+    #     0.0005,
+    #     0.0008,
+    #     0.001,
+    #     0.0015,
+    #     0.002,
+    #     0.003,
+    #     0.01,
+    #     0.1,
+    #     0.3,
+    #     0.5,
+    #     1.0,
+    # ]
+    epsilons = list(range(20))
+    epsilons = [x/20 for x in epsilons]
+    epsilons = [x*256 for x in epsilons]
+    print('labels', labels.size())
+    raw_advs, clipped_advs, success = attack(fmodel, inputs=imgs, criterion=labels, epsilons=epsilons)
+    robust_accuracy = 1 - success.float().mean(axis=-1)
+    robust_accs.append(robust_accuracy)
+
+    if opt.out_file is not None:
+        for (eps, acc) in zip(epsilons, robust_accuracy):
+            out_file.write(f"{eps:<6} {acc.item() * 100:4.1f}\n")
+
+    else:
+        print("robust accuracy for perturbations with")
+        for (eps, acc) in zip(epsilons, robust_accuracy):
+            print(f"  Linf norm ≤ {eps:<6}: {acc.item() * 100:4.1f} %")
+        print()
