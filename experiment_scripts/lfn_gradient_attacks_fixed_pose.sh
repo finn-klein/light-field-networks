711e65b4629eb557a60065f5a23277e7793ffa4b
finn-klein <finn.klein@outlook.com>
2023-08-24 11:14:07 +0200

    adj eps

---
+++ experiment_scripts/lfn_gradient_attacks_fixed_pose.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:a100:1
+#SBATCH --partition=a100
+#SBATCH --job-name=lfn_gradient_attacks
+#SBATCH --export=None
+
+module load python
+module load cuda/11.3.1
+module load git
+source activate lf
+
+mkdir adv_attacks/$SLURM_JOB_ID
+
+#for y in {0..12}; do
+python experiment_scripts/lfn_adversarial.py --data_root /home/woody/iwi9/iwi9015h/nmr/NMR_Dataset \
+--checkpoint_path /home/vault/iwi9/iwi9015h/experiments/train00/nmr/64_128_None/checkpoints/model_epoch_0006_iter_040000.pth \
+--num_inference_iters 500 \
+--num_instances_per_class 256 \
+--batch_size 256 \
+--lr 5e-4 \
+--single_class_string 0 \
+--adv_epsilon 0.001 \
+--out_folder adv_attacks/$SLURM_JOB_ID
+#done
