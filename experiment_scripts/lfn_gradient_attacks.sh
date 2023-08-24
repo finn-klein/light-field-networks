4f809dec564de02e63795ca4402043fc8520b697
finn-klein <finn.klein@outlook.com>
2023-08-24 11:19:23 +0200

    adj eps

--- experiment_scripts/lfn_gradient_attacks.sh
+++ experiment_scripts/lfn_gradient_attacks.sh
@@ -21,6 +21,6 @@ python experiment_scripts/lfn_adversarial.py --data_root /home/woody/iwi9/iwi901
 --batch_size 256 \
 --lr 5e-4 \
 --single_class_string 0 \
---adv_epsilon 0.01 \
+--adv_epsilon 0.005 \
 --out_folder adv_attacks/$SLURM_JOB_ID
 #done
