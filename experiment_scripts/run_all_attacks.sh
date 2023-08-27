3fcf5eea9b1d6e491995526910d649b67d436d2e
finn-klein <finn.klein@outlook.com>
2023-08-27 15:18:44 +0200

    update ff_ckpt_path

--- experiment_scripts/run_all_attacks.sh
+++ experiment_scripts/run_all_attacks.sh
@@ -26,7 +26,7 @@ script_path="/home/woody/iwi9/iwi9015h/light-field-networks/experiment_scripts"
 lfn_root_path="/home/woody/iwi9/iwi9015h/nmr/NMR_Dataset"
 lfn_ckpt_path="/home/vault/iwi9/iwi9015h/experiments/train00/nmr/64_128_None/checkpoints/model_epoch_0006_iter_040000.pth"
 ff_root_path="/home/woody/iwi9/iwi9015h/nmr_ff_test_2"
-ff_ckpt_path="/home/vault/iwi9/iwi9015h/experiments/train_baseline00/checkpoints/best.pth"
+ff_ckpt_path="/home/vault/iwi9/iwi9015h/experiments/train_baseline01/checkpoints/best.pth"
 
 echo "experiment folder: $1"
 
