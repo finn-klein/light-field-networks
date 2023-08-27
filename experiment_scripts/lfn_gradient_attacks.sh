8cb502128adf96a5468ec2852f20a5e67dfe4e3c
finn-klein <finn.klein@outlook.com>
2023-08-27 15:49:59 +0200

    update

--- experiment_scripts/lfn_gradient_attacks.sh
+++ experiment_scripts/lfn_gradient_attacks.sh
@@ -11,16 +11,15 @@ module load cuda/11.3.1
 module load git
 source activate lf
 
-mkdir adv_attacks/$SLURM_JOB_ID
+mkdir $1
 
-#for y in {0..12}; do
-python experiment_scripts/lfn_adversarial.py --data_root /home/woody/iwi9/iwi9015h/nmr/NMR_Dataset \
---checkpoint_path /home/vault/iwi9/iwi9015h/experiments/train00/nmr/64_128_None/checkpoints/model_epoch_0006_iter_040000.pth \
---num_inference_iters 500 \
---num_instances_per_class 256 \
---batch_size 256 \
---lr 5e-4 \
---single_class_string 0 \
---adv_epsilon 0 \
---out_folder adv_attacks/$SLURM_JOB_ID
+for class in {0..12}; do
+    python experiment_scripts/lfn_adversarial.py --data_root /home/woody/iwi9/iwi9015h/nmr/NMR_Dataset \
+    --checkpoint_path /home/vault/iwi9/iwi9015h/experiments/train00/nmr/64_128_None/checkpoints/model_epoch_0006_iter_040000.pth \
+    --num_inference_iters 500 \
+    --num_instances_per_class 256 \
+    --batch_size 256 \
+    --lr 5e-4 \
+    --single_class_string ${class} \
+    --out_folder $1/${class}
 #done
