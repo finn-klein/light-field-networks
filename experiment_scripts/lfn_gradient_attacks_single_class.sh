6ca1e91ab36776585cc0519bf33482784c9b83b6
finn-klein <finn.klein@outlook.com>
2023-08-29 09:15:06 +0200

    add

---
+++ experiment_scripts/lfn_gradient_attacks_single_class.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:a100:1
+#SBATCH --partition=a100
+#SBATCH --job-name=lfn_gradient_attacks_single_class
+#SBATCH --export=None
+
+module load python
+module load cuda/11.3.1
+module load git
+source activate lf
+
+mkdir $1
+
+    python experiment_scripts/lfn_adversarial.py --data_root /home/woody/iwi9/iwi9015h/nmr/NMR_Dataset \
+    --checkpoint_path /home/vault/iwi9/iwi9015h/experiments/train00/nmr/64_128_None/checkpoints/model_epoch_0006_iter_040000.pth \
+    --num_inference_iters 500 \
+    --num_instances_per_class 256 \
+    --batch_size 256 \
+    --lr 5e-4 \
+    --single_class_string 0 \
+    --out_folder $1 \
+    --out_file 0
