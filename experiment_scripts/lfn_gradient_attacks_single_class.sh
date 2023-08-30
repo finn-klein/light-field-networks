296e2ceb47dffa2776da2d8ca2d52f9c69efee7f
finn-klein <finn.klein@outlook.com>
2023-08-30 14:57:16 +0200

    set batch size to 128

--- experiment_scripts/lfn_gradient_attacks_single_class.sh
+++ experiment_scripts/lfn_gradient_attacks_single_class.sh
@@ -16,8 +16,8 @@ mkdir $1
     python experiment_scripts/lfn_adversarial.py --data_root /home/woody/iwi9/iwi9015h/nmr/NMR_Dataset \
     --checkpoint_path /home/vault/iwi9/iwi9015h/experiments/train00/nmr/64_128_None/checkpoints/model_epoch_0006_iter_040000.pth \
     --num_inference_iters 500 \
-    --num_instances_per_class 256 \
-    --batch_size 256 \
+    --num_instances_per_class 128 \
+    --batch_size 128 \
     --lr 5e-4 \
     --single_class_string 0 \
     --out_folder $1 \
